f5737076fc7994a861a15b4dffa8f0d2ec730e21
Aristela Marku <aristela.marku@gmail.com>
2021-11-02 13:29:36 +0100

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
+    
+    #setter
+    def name=(name)
+        @name=name
+    end
+    #getter
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job=job
+    end
+
+    def job
+        @job
+    end
 
 end
+
+kanye=Person.new
+kanye.name = "Kanye"
+kanye.name
